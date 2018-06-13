665a50d9baf576eed1afab6559daf537286b4157
Alex <agervs@gmail.com>
2018-06-13 12:47:44 -0400

    First commit. Nano sends ooooo message successfully to Feather.

---
+++ sender-nano-test.ino
@@ -0,0 +1,31 @@
+// Wire Slave Sender
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Sends data as an I2C/TWI slave device
+// Refer to the "Wire Master Reader" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+String iconStatus = "oooooo";
+
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+}
+
+void loop() {
+
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write(iconStatus.c_str()); // respond with message of 6 bytes
+  // as expected by master
+}
